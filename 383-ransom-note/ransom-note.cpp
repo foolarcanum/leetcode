c179e85522af1db2f9c502a872ae92ba2be8fcd2
foolarcanum <87370688+foolarcanum@users.noreply.github.com>
2024-09-19 22:57:52 +0300

    Time: 35 ms (5.91%) | Memory: 11.5 MB (56.20%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int> mag;
+        for(int i=0;i<magazine.length();i++){
+            if(mag[magazine[i]]){mag[magazine[i]]++;}
+            else{mag[magazine[i]]=1;}
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            if(mag[ransomNote[i]]){mag[ransomNote[i]]--;}
+            else{return false;}
+        }
+        return true;    
+    }
+};
